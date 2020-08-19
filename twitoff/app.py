59c67012537aae2159b143c60da9ed69161ca234
Andrew Arnett <andrewarnett2323@gmail.com>
2020-08-19 10:42:05 -0700

    U3S3M3 Assignments

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,7 @@
 """ Main app/routing file for Twitoff."""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User
+from .predict import predict_user
 from .twitter import insert_example_users
 
 
@@ -16,11 +17,40 @@ def create_app():
     def root():
         return render_template('base.html', title='Home',
                                users=User.query.all())
+    
+    app.route('/user', methods=['POST'])
+    app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
+
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user1, user2 = sorted([request.values['user1'],
+                               request.values['user2']])
+        if user1 == user2:
+            message = 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            message = '"{}" is more likely to be said by {} than {}'.format(
+                request.values['tweet_text'], user1 if prediction else user2,
+                user2 if prediction else user1)
+        return render_template('prediction.html', title='Prediction', message=message)
 
     @app.route('/update')
     def update():
         #Reset the database
-        insert_example_users()
+        insert_example_users() # Optonal - update existing users
         return render_template('base.html', title='Users updated!',
                                users=User.query.all())
 
